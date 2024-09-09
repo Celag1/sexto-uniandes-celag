c86c566de764aebe711da3f84486352e0dfa0408
Celso Aguirre <ingsoft7@hotmail.com>
2024-09-08 21:16:54 -0500

    Resp
    Resp

--- 04Plantilla/src/app/facturas/nuevafactura/nuevafactura.component.ts
+++ 04Plantilla/src/app/facturas/nuevafactura/nuevafactura.component.ts
@@ -1,32 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router, ActivatedRoute } from '@angular/router';
+import { Router, Event } from '@angular/router';
 import { IFactura } from 'src/app/Interfaces/factura';
 import { ICliente } from 'src/app/Interfaces/icliente';
 import { ClientesService } from 'src/app/Services/clientes.service';
 import { FacturaService } from 'src/app/Services/factura.service';
-import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-nuevafactura',
   standalone: true,
-  imports: [FormsModule, ReactiveFormsModule, CommonModule],
+  imports: [FormsModule, ReactiveFormsModule],
   templateUrl: './nuevafactura.component.html',
-  styleUrls: ['./nuevafactura.component.scss']
+  styleUrl: './nuevafactura.component.scss'
 })
 export class NuevafacturaComponent implements OnInit {
+  //variables o constantes
   titulo = 'Nueva Factura';
   listaClientes: ICliente[] = [];
   listaClientesFiltrada: ICliente[] = [];
   totalapagar: number = 0;
+  //formgroup
   frm_factura: FormGroup;
-  idFactura: number;
 
+  ///////
   constructor(
-    private clientesServicios: ClientesService,
+    private clietesServicios: ClientesService,
     private facturaService: FacturaService,
-    private navegacion: Router,
-    private rutaActiva: ActivatedRoute
+    private navegacion: Router
   ) {}
 
   ngOnInit(): void {
@@ -38,7 +38,7 @@ export class NuevafacturaComponent implements OnInit {
       Clientes_idClientes: new FormControl('', Validators.required)
     });
 
-    this.clientesServicios.todos().subscribe({
+    this.clietesServicios.todos().subscribe({
       next: (data) => {
         this.listaClientes = data;
         this.listaClientesFiltrada = data;
@@ -47,29 +47,10 @@ export class NuevafacturaComponent implements OnInit {
         console.log(e);
       }
     });
-
-    this.idFactura = this.rutaActiva.snapshot.params['id'];
-    if (this.idFactura) {
-      this.facturaService.uno(this.idFactura).subscribe({
-        next: (factura: IFactura) => {
-          console.log('Datos de la factura cargada:', factura);
-          this.titulo = 'Actualizar Factura';
-          this.frm_factura.patchValue({
-            Fecha: new Date(factura.Fecha).toISOString().split('T')[0], // Asegura que la fecha tenga el formato correcto
-            Sub_total: factura.Sub_total,
-            Sub_total_iva: factura.Sub_total_iva,
-            Valor_IVA: factura.Valor_IVA,
-            Clientes_idClientes: factura.Clientes_idClientes
-          });
-          this.calculos();
-        }
-      });
-    }
   }
 
   grabar() {
     let factura: IFactura = {
-      idFactura: this.idFactura,
       Fecha: this.frm_factura.get('Fecha')?.value,
       Sub_total: this.frm_factura.get('Sub_total')?.value,
       Sub_total_iva: this.frm_factura.get('Sub_total_iva')?.value,
@@ -77,23 +58,13 @@ export class NuevafacturaComponent implements OnInit {
       Clientes_idClientes: this.frm_factura.get('Clientes_idClientes')?.value
     };
 
-    if (this.idFactura) {
-      this.facturaService.actualizar(factura).subscribe((respuesta) => {
-        if (parseInt(respuesta) > 0) {
-          alert('Factura actualizada');
-          this.navegacion.navigate(['/facturas']);
-        }
-      });
-    } else {
-      this.facturaService.insertar(factura).subscribe((respuesta) => {
-        if (parseInt(respuesta) > 0) {
-          alert('Factura grabada');
-          this.navegacion.navigate(['/facturas']);
-        }
-      });
-    }
+    this.facturaService.insertar(factura).subscribe((respuesta) => {
+      if (parseInt(respuesta) > 0) {
+        alert('Factura grabada');
+        this.navegacion.navigate(['/facturas']);
+      }
+    });
   }
-
   calculos() {
     let sub_total = this.frm_factura.get('Sub_total')?.value;
     let iva = this.frm_factura.get('Valor_IVA')?.value;
@@ -102,8 +73,8 @@ export class NuevafacturaComponent implements OnInit {
     this.totalapagar = parseInt(sub_total) + sub_total_iva;
   }
 
-  cambio(objetoSelect: any) {
-    let idCliente = objetoSelect.target.value;
+  cambio(objetoSleect: any) {
+    let idCliente = objetoSleect.target.value;
     this.frm_factura.get('Clientes_idClientes')?.setValue(idCliente);
-  }
+  }
 }
